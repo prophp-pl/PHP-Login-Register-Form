376ec1ccab4563afed42c8f113eb9006748d6126
wwwgo-pl <robert@wwwgo.pl>
2016-01-08 15:37:55 +0100

    Update login.php

--- views/login.php
+++ views/login.php
@@ -12,7 +12,7 @@ $helper = $factory->newInstance();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="wwwgo.pl">
 
-    <title>Formularz rejestracji</title>
+    <title>Formularz logowania</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
